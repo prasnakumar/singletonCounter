ab73ac1c33d83b6b48e13f8a465520097f1eb0dc
Prasanna Kumar N <Prasanna.Augmento10@gmail.com>
2022-07-15 01:20:50 +0530

    Add project files.

---
+++ Counter/Program.cs
@@ -0,0 +1,57 @@
+﻿using Singleton;
+using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = true;
+
+            Counter_singleton counter = Counter_singleton.ObjectCreate;//Creates 1 only one instance 
+            
+           
+            Counter_singleton counter1 = Counter_singleton.ObjectCreate;//Creates  2 but only one instance 
+
+          
+
+
+
+            while (options)
+            {
+                Console.WriteLine("please select the following action \n1: Increment  \n2: Decrement  \n3: Display Number \n0: Exit ");
+                var select = Console.ReadLine();
+                //this can be changes to switch 
+                if ( select == "1")
+                {
+                    counter.Increment();
+                }
+                else if (select == "2")
+                {
+                    counter.Decrement();
+                }
+                else if (select == "3")
+                {
+                  Console.WriteLine("current counter "+Counter_singleton.instance.Count);
+                }
+                else if (select == "0")
+                {
+                    Console.WriteLine("Bye.... ");
+                    options = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Option....");
+                   
+                }
+            }
+            
+            
+
+           
+        }
+    }
+
+    
+}
---
+++ Singleton/Counter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Singleton
+{
+    public  class Counter_singleton
+    {
+       public    int Count = 0;
+        public static Counter_singleton instance = null;
+        public string Value { get; set; }
+
+
+        private Counter_singleton()
+        {
+            Console.WriteLine("current intial value " + Count);
+        }
+
+        
+        public static Counter_singleton ObjectCreate
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance =  new Counter_singleton();
+                }
+                return instance;
+            }
+        }
+        public void Decrement()
+        {
+            
+            Console.WriteLine("Action Decrement Count is " + --Count);
+           
+        }
+        public void Increment()
+        {
+            
+            Console.WriteLine("Action Increment Count  is  " + ++Count);
+           
+        }
+        
+        
+    }
+}
